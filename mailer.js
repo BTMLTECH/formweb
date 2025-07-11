23a2b79915253c547a082c143ff4924f8e20192c
TobiLight <jgetitdonefast@gmail.com>
2025-07-11 01:49:36 +0100

    last one

--- mailer.js
+++ mailer.js
@@ -14,7 +14,7 @@ async function sendMailToUser({ email, subject, template, data }) {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: parseInt(process.env.EMAIL_PORT),
-    secure: false,
+    secure: parseInt(process.env.EMAIL_PORT) === 465,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
